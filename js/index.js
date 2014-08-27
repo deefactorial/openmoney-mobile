94495e49813148856c58db815baa5233efe4d754
deefactorial <deefactorial@gmail.com>
2014-08-27 08:27:14 -0700

    adjust logging and alerts

--- js/index.js
+++ js/index.js
@@ -55,7 +55,7 @@ function onDeviceReady() {
 	            
 	            
 	            if (tag.isWritable && tag.canMakeReadOnly) {
-	            	alert(JSON.stringify(tag));
+	            	log(JSON.stringify(tag));
 	            	
 	            	var type = "com.openmoney.mobile/json",
 	                id = config.user.user_id,
@@ -75,10 +75,10 @@ function onDeviceReady() {
 
 	            // assuming the first record in the message has 
 	            // a payload that can be converted to a string.
-	            alert(nfc.bytesToString(ndefMessage[0].payload).substring(3));
+	            alert(nfc.bytesToString(ndefMessage[0].payload));
 	        }, 
 	        function () { // success callback
-	            alert("Waiting for NDEF tag");
+	            log("Waiting for NDEF tag");
 	        },
 	        function (error) { // error callback
 	            alert("Error adding NDEF listener " + JSON.stringify(error));
@@ -96,11 +96,11 @@ function onDeviceReady() {
 	    // dump the raw json of the message
 	    // note: real code will need to decode
 	    // the payload from each record
-	    alert(JSON.stringify(tag));
+	    log(JSON.stringify(tag));
 	
 	    // assuming the first record in the message has 
 	    // a payload that can be converted to a string.
-	    alert(nfc.bytesToString(ndefMessage[0].payload).substring(3));
+	    alert(nfc.bytesToString(ndefMessage[0].payload));
 	}, function () {
 		//success callback
 	}, function () {
