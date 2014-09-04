adc95886cbada4e13ff0d0a2cd58a48e1e0352f8
deefactorial <deefactorial@gmail.com>
2014-09-04 10:41:32 -0700

    add equals prototype to arrays

--- js/model.js
+++ js/model.js
@@ -1,3 +1,32 @@
+
+// source: https://stackoverflow.com/questions/7837456/comparing-two-arrays-in-javascript
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+    // if the other array is a false value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;       
+        }           
+        else if (this[i] != array[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+}
+
+
+
 // Parent class constructor
 function Parent() {
   this.a = 42;
