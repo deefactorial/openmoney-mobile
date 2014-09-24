e8586db722210314a6cb057c16214b9c7e02d864
deefactorial <deefactorial@gmail.com>
2014-09-24 10:24:59 -0700

    select header correctly

--- js/index.js
+++ js/index.js
@@ -57,13 +57,13 @@ function doOnOrientationChange () {
       case -90:
       case 90:
         log('landscape');
-        $("header").css('display','inline-block');
-        setTimeout( function () { $("header").css('display','block'); }, 0);
+        $(":header").css('display','inline-block');
+        setTimeout( function () { $(":header").css('display','block'); }, 0);
         break; 
       default:
         log('portrait');
-      	$("header").css('display','inline-block');
-      	setTimeout( function () { $("header").css('display','block'); }, 0);
+      	$(":header").css('display','inline-block');
+      	setTimeout( function () { $(":header").css('display','block'); }, 0);
         break; 
     }
 }
