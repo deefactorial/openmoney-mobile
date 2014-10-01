a4239da09bbc8680d83ac083eb536700b651dbc9
deefactorial <deefactorial@gmail.com>
2014-10-01 10:56:10 -0700

    update query

--- js/index.js
+++ js/index.js
@@ -3074,7 +3074,7 @@ function destroyBeamTag(cb) {
         if (error) { return cb( error ) }
         
         config.views( [ "account_details", {
-            startkey : [ {} ], endkey : [], descending : true
+           descending : true, include_docs : true
         } ], function(error, transactions) {
         	if (error) { return cb( error ) }
         	
