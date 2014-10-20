a5b6954edbb59fd8fc662604afe5a4f2e243a819
deefactorial <deefactorial@gmail.com>
2014-10-20 09:16:07 -0700

    spell them the same

--- js/index.js
+++ js/index.js
@@ -3034,7 +3034,7 @@ function getTradingName(row, doc, trading_names, callback) {
 	if (typeof doc[capacityName] != 'undefined' && doc[capacityName] != '' && doc[capacityName] != null) {
 		alert(doc[capacityName]);
 		trading_name.capacity = parseFloat( doc[capacityName] );
-		if (! isNumeric( trading_name.capacity ) || trading_name.capacity == null || typeof  trading_name.capacity == 'undefined') {
+		if (! isNumberic( trading_name.capacity ) || trading_name.capacity == null || typeof  trading_name.capacity == 'undefined') {
 			$("input[name='" + capacityName + "']").attr("pattern","not-fail").focus();
 			navigator.notification.alert( "Could not parse number."  , function() {  }, "Not a Number", "OK")
 			callback('Not a Number')
