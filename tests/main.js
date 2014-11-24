7968154fb077bc5fa6cc775a0f84440bd4d8336e
deefactorial <deefactorial@gmail.com>
2014-11-24 13:46:09 -0800

    try after ondeviceready

--- tests/main.js
+++ tests/main.js
@@ -1,6 +1,6 @@
 asyncTest( "Openmoney Mobile App Tests", function() {
 	expect(1);
-	window.goIndex().then(function(){
+	window.onDeviceReady().then(function(){
 		equal($('#content').contents().find('h1.topcoat-navigation-bar__title').html(), "openmoney");
 		start();
 	})
