d312ce2620105934cbeb412f71bdce3ea599f8b6
deefactorial <deefactorial@gmail.com>
2014-12-08 11:53:16 -0800

    do a typeof check

--- js/index.js
+++ js/index.js
@@ -208,7 +208,7 @@ function setupConfig(done) {
         	}
         } else {
             if (SERVER_LOGIN) {
-                if (config.user.name) {
+                if (typeof window.config.user.name != 'undefined') {
                     if (newUser.sessionID == '') {
                         return cb()
                     } else {
