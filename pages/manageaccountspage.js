9a25998fb2eb547a16b758d8cda6827269051775
deefactorial <deefactorial@gmail.com>
2015-01-02 16:33:38 -0800

    try $(this)

--- pages/manageaccountspage.js
+++ pages/manageaccountspage.js
@@ -31,7 +31,7 @@ function goManageAccounts(parameters) {
 			var myOptions = {};
 			$( "#scrollable li.trading_names" ).each( function(key, trading_name){
 				console.log("setting lib for trading name" + JSON.stringify([key, trading_name ]))
-				var hammertime = new Hammer(trading_name, myOptions);
+				var hammertime = new Hammer($(this), myOptions);
 				hammertime.on('swiperight', function(ev) {
 				    console.log(ev);
 				});
