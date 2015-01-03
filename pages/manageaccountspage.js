484996de411a628ab8b2145effb34f995d8a3e07
deefactorial <deefactorial@gmail.com>
2015-01-02 16:53:13 -0800

    get the string name

--- pages/manageaccountspage.js
+++ pages/manageaccountspage.js
@@ -30,7 +30,7 @@ function goManageAccounts(parameters) {
 			console.log("setting hammer lib");
 			var myOptions = {};
 			$( "#scrollable li.trading_names" ).each( function(key, trading_name){
-				console.log("setting lib for trading name" + $(this).get(0))
+				console.log("setting lib for trading name" + $(this).get(0).toString())
 				var hammertime = new Hammer($(this).get(0));
 				hammertime.on('swiperight', function(ev) {
 				    console.log(ev);
