b15aec5fa2cd26cef71001c9879444683e5d20aa
deefactorial <deefactorial@gmail.com>
2015-02-20 11:01:20 -0800

    use the user password not the session token for the view

--- js/index.js
+++ js/index.js
@@ -589,7 +589,7 @@ function setupConfig(done) {
 	    } else {
 	    	var design = "_design/dev_openmoney" ;//+ new Date().getTime();
 	    	//query the local server for the views since the sync_gateway doesn't support _design docs.
-	    	var views = coax( { "uri": REMOTE_SYNC_PROTOCOL + REMOTE_SYNC_SERVER + "/" + appDbName + "/" , "auth" : { "username" : window.config.user.name, "password": window.config.user.session_token } } );
+	    	var views = coax( { "uri": REMOTE_SYNC_PROTOCOL + REMOTE_SYNC_SERVER + "/" + appDbName + "/" , "auth" : { "username" : window.config.user.name, "password": window.config.user.password } } );
 	    	//var views = coax( { "uri": REMOTE_SYNC_PROTOCOL + REMOTE_SYNC_SERVER + "/" + appDbName + "/" } );
 	    	cb( false , views( [ design, "_view" ] ) )
 	    }
