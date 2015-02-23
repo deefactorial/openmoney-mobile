093e367204c2995e765781ad6b3a3c3654f61f44
deefactorial <deefactorial@gmail.com>
2015-02-23 10:56:34 -0800

    write a function that deletes all cookies in the browser

--- pages/loginpage.js
+++ pages/loginpage.js
@@ -256,12 +256,27 @@ function doServerLogout(callBack) {
             } else {
             	config.db = null;
                 config.views = null;
+                deleteAllCookies();
                 setupConfig( function(error, ok) {
                     callBack( error, result )
                 } )
             }
         } )
     } )
+}
+
+
+
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+    	var cookie = cookies[i];
+    	var eqPos = cookie.indexOf("=");
+    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
 }
 
 
