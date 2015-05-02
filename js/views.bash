66b23a3483fe0eeb2e1bc3eca7fa31b0426b9c61
deefactorial <deefactorial@gmail.com>
2015-05-02 09:56:29 -0700

    Views support

--- js/views.bash
+++ js/views.bash
@@ -1,4 +1,4 @@
 #execute from server
-curl -X PUT -H "Content-type: application/json" localhost:4985/openmoney_shadow/_design/dev_rest --data @views.js
+curl -X PUT -H "Content-type: application/json" localhost:4985/openmoney_shadow/_design/dev_rest --data @views
 #test:
 curl localhost:4985/openmoney_shadow/_design/dev_rest/_view/account_balance
