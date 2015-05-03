48a3ea1fbac12e8bcad7cd615d8ecaa8410b26cf
deefactorial <deefactorial@gmail.com>
2015-05-03 13:47:26 -0700

    resetting views into sync gateway

--- js/views.bash
+++ js/views.bash
@@ -1,4 +1,4 @@
 #execute from server
-curl -X PUT -H "Content-type: application/json" https://localhost:4985/openmoney_shadow/_design/dev_rest --data @views
+curl -X PUT -H "Content-type: application/json" https://cloud.openmoney.cc:4985/openmoney_shadow/_design/dev_rest --data @views
 #test:
-curl https://localhost:4985/openmoney_shadow/_design/dev_rest/_view/account_balance
+curl https://cloud.openmoney.cc:4985/openmoney_shadow/_design/dev_rest/_view/account_balance
